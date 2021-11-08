18251932cf435e4994cde9cfad82594895e93350
Buzzard765 <bhaswaraa@yahoo.com>
2021-11-08 22:56:57 +0700

    Tetris for Android

---
+++ Assets/Educational Games/Tetris/Core.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    public static int width = 6;
+    public static int heigth = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool insideGrid(Vector2 pos) {
+        return ((int)pos.x >= -6 && (int)pos.x < width && (int)pos.y >= -10);
+    }
+
+    public Vector2 Round(Vector2 pos) {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    public void spawnTetrimino() {
+        GameObject NextTtmn = (GameObject)Instantiate(
+            Resources.Load(randomName(), typeof(GameObject)),  new Vector2(5, 20), Quaternion.identity
+            );
+    }
+
+    string randomName(){
+        int ttmnIndex = Random.Range(1, 8);
+        string ttmnName = "T";
+        switch (ttmnIndex) {
+            case 1:
+                ttmnName = "I";
+                break;
+            case 2:
+                ttmnName = "O";
+                break;
+            case 3:
+                ttmnName = "S";
+                break;
+            case 4:
+                ttmnName = "Z";
+                break;
+            case 5:
+                ttmnName = "L";
+                break;
+            case 6:
+                ttmnName = "J";
+                break;
+            case 7:
+                ttmnName = "T";
+                break;            
+        }
+        return ttmnName;
+        
+    }
+}
---
+++ Assets/Educational Games/Tetris/Tetrimino.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetrimino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallspeed = 0.5f;
+
+    public bool canRotate = false, limitRotaton = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        Drop(1);                  
+    }
+
+    //controls for android
+    void CheckInputPC() {
+    
+    }
+
+    public void MoveXPos(float x ) {
+        transform.position += new Vector3(x, 0, 0);
+        if (checkPos())
+        {
+
+        }
+        else {
+            transform.position += new Vector3(-x, 0, 0);
+        }
+    }
+
+    public void RotatePos() {
+        if (canRotate == true) {
+            if (limitRotaton == true)
+            {
+                if (transform.rotation.eulerAngles.z >= 90)
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+                else {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            else {
+                transform.Rotate(0, 0, 90);
+            }
+            if (checkPos())
+            {
+
+            }
+            else
+            {
+                if (limitRotaton == true)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else {
+                    transform.Rotate(0, 0, -90);
+                }         
+            }
+        }
+        //transform.Rotate(0, 0, 90);       
+    }
+
+    public void Drop(float y)
+    {
+        if (Time.time - fall >= fallspeed)
+        {
+            transform.position += new Vector3(0, -y, 0);
+            if (checkPos())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, y, 0);
+            }
+            fall = Time.time;
+            Debug.Log(fall);
+        }        
+    }
+
+    bool checkPos() {
+        foreach (Transform mino in transform) {
+            Vector2 pos = FindObjectOfType<Core>().Round(mino.position);
+
+            if (FindObjectOfType<Core>().insideGrid(pos) == false)
+            {
+                return false;
+            }
+            else {
+
+            }
+            
+        }
+        return true;
+    }
+
+}
