09272d5b4875eb8fab9870abadcb2f4dde14e21b
Buzzard765 <bhaswaraa@yahoo.com>
2021-11-17 22:08:02 +0700

    new sub game - Whack a Mole

---
+++ Assets/Educational Games/Whack A Mole/Mole.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Mole : MonoBehaviour
+{
+    private float OutGroundTime;
+    private float UnderGround;
+    public float startUG, startOG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OutGroundTime = Random.Range(0.5f, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf == true) {
+            if (OutGroundTime < 0)
+            {
+                DeSpawn();               
+            }
+            else {
+                OutGroundTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    
+    void DeSpawn() {
+        gameObject.SetActive(false);
+        OutGroundTime = Random.Range(0.5f, 3);
+        
+    }
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Gotcha");
+            DeSpawn();
+            FindObjectOfType<MoleSpawning>().score_get += 1;
+            FindObjectOfType<MoleSpawning>().time_get += 1;
+        }
+    }
+
+    IEnumerator KnockOut() {
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ Assets/Educational Games/Whack A Mole/MoleSpawning.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleSpawning : MonoBehaviour
+{
+    [System.Serializable] public class MoleHole {
+        public Transform hole;
+        public bool hasMole;
+    }
+
+    public GameObject Mole;
+    //public Transform[] hole;
+    //public bool hasMole;
+    public List<GameObject> AllHoles = new List<GameObject>();
+    public float spawnRate;
+    private float currentspawnRate;
+    private int randomSpot;
+    private int score;
+    [SerializeField]private float time;
+
+    public float time_get {
+        get { return time; }
+        set { time = value; }
+    }
+    public int score_get
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentspawnRate = spawnRate;
+        randomSpot = Random.Range(0, AllHoles.Count);
+        foreach (GameObject holes in AllHoles) {
+            holes.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0) {
+            RandomizeSpawn();
+        }
+        time -= Time.deltaTime;
+        
+    }
+
+    void RandomizeSpawn() {
+        if (currentspawnRate <= 0 && AllHoles[randomSpot].activeSelf == false)
+        {
+            spawnMole(randomSpot);
+            randomSpot = Random.Range(0, AllHoles.Count);
+            currentspawnRate = spawnRate;
+        }
+        else if (AllHoles[randomSpot].activeSelf == true)
+        {
+            randomSpot = Random.Range(0, AllHoles.Count);
+        }
+        else
+        {
+            currentspawnRate -= Time.deltaTime;
+        }
+    }
+
+    void spawnMole(int index) {       
+        AllHoles[index].SetActive(true);
+    }
+}
